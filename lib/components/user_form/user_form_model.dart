d9126115bf241fe4324947709ed24365d50bab61
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2025-03-06 19:07:33 +0000

    feat: #9  apenas nome como obrigatorio na criação de usuario

--- lib/components/user_form/user_form_model.dart
+++ lib/components/user_form/user_form_model.dart
@@ -22,47 +22,18 @@ class UserFormModel extends FlutterFlowModel<UserFormWidget> {
   FocusNode? emailFocusNode;
   TextEditingController? emailTextController;
   String? Function(BuildContext, String?)? emailTextControllerValidator;
-  String? _emailTextControllerValidator(BuildContext context, String? val) {
-    if (val == null || val.isEmpty) {
-      return 'Preencha esse campo';
-    }
-
-    if (!RegExp(kTextValidatorEmailRegex).hasMatch(val)) {
-      return 'Has to be a valid email address.';
-    }
-    return null;
-  }
-
   // State field(s) for phone widget.
   FocusNode? phoneFocusNode;
   TextEditingController? phoneTextController;
   String? Function(BuildContext, String?)? phoneTextControllerValidator;
-  String? _phoneTextControllerValidator(BuildContext context, String? val) {
-    if (val == null || val.isEmpty) {
-      return 'Preencha esse campo';
-    }
-
-    return null;
-  }
-
   // State field(s) for cpf widget.
   FocusNode? cpfFocusNode;
   TextEditingController? cpfTextController;
   String? Function(BuildContext, String?)? cpfTextControllerValidator;
-  String? _cpfTextControllerValidator(BuildContext context, String? val) {
-    if (val == null || val.isEmpty) {
-      return 'Preencha esse campo';
-    }
-
-    return null;
-  }
 
   @override
   void initState(BuildContext context) {
     nameTextControllerValidator = _nameTextControllerValidator;
-    emailTextControllerValidator = _emailTextControllerValidator;
-    phoneTextControllerValidator = _phoneTextControllerValidator;
-    cpfTextControllerValidator = _cpfTextControllerValidator;
   }
 
   @override
