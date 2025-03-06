351a959c769fefb70c442408b1d9038802257206
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2025-03-06 19:53:40 +0000

    fix: #11 remove query do botão de excluir e passa chamada para form

--- lib/components/dialog_delete_book/dialog_delete_book_widget.dart
+++ lib/components/dialog_delete_book/dialog_delete_book_widget.dart
@@ -49,91 +49,94 @@ class _DialogDeleteBookWidgetState extends State<DialogDeleteBookWidget> {
       ),
       child: Padding(
         padding: EdgeInsetsDirectional.fromSTEB(24.0, 24.0, 24.0, 24.0),
-        child: Container(
-          width: 400.0,
-          decoration: BoxDecoration(
-            color: FlutterFlowTheme.of(context).secondaryBackground,
-            borderRadius: BorderRadius.circular(12.0),
+        child: FutureBuilder<List<GetLoanByIdRow>>(
+          future: SQLiteManager.instance.getLoanById(
+            bookId: widget.id,
           ),
-          child: Padding(
-            padding: EdgeInsets.all(16.0),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Row(
-                  mainAxisSize: MainAxisSize.max,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          builder: (context, snapshot) {
+            // Customize what your widget looks like when it's loading.
+            if (!snapshot.hasData) {
+              return Center(
+                child: SizedBox(
+                  width: 50.0,
+                  height: 50.0,
+                  child: CircularProgressIndicator(
+                    valueColor: AlwaysStoppedAnimation<Color>(
+                      FlutterFlowTheme.of(context).primary,
+                    ),
+                  ),
+                ),
+              );
+            }
+            final containerGetLoanByIdRowList = snapshot.data!;
+
+            return Container(
+              width: 400.0,
+              decoration: BoxDecoration(
+                color: FlutterFlowTheme.of(context).secondaryBackground,
+                borderRadius: BorderRadius.circular(12.0),
+              ),
+              child: Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
                   children: [
-                    Text(
-                      'Excluir Livro',
-                      style:
-                          FlutterFlowTheme.of(context).headlineSmall.override(
+                    Row(
+                      mainAxisSize: MainAxisSize.max,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'Excluir Livro',
+                          style: FlutterFlowTheme.of(context)
+                              .headlineSmall
+                              .override(
                                 fontFamily: 'Inter Tight',
                                 letterSpacing: 0.0,
                               ),
+                        ),
+                      ],
                     ),
-                  ],
-                ),
-                Text(
-                  'Tem certeza que deseja excluir este livro? Esta ação não poderá ser desfeita.',
-                  style: FlutterFlowTheme.of(context).bodyMedium.override(
-                        fontFamily: 'Inter',
-                        letterSpacing: 0.0,
-                      ),
-                ),
-                Row(
-                  mainAxisSize: MainAxisSize.max,
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    FFButtonWidget(
-                      onPressed: () async {
-                        Navigator.pop(context);
-                      },
-                      text: 'Cancelar',
-                      options: FFButtonOptions(
-                        width: 120.0,
-                        height: 40.0,
-                        padding: EdgeInsets.all(8.0),
-                        iconPadding:
-                            EdgeInsetsDirectional.fromSTEB(0.0, 0.0, 0.0, 0.0),
-                        color: FlutterFlowTheme.of(context).secondaryBackground,
-                        textStyle:
-                            FlutterFlowTheme.of(context).bodyMedium.override(
+                    Text(
+                      'Tem certeza que deseja excluir este livro? Esta ação não poderá ser desfeita.',
+                      style: FlutterFlowTheme.of(context).bodyMedium.override(
+                            fontFamily: 'Inter',
+                            letterSpacing: 0.0,
+                          ),
+                    ),
+                    Row(
+                      mainAxisSize: MainAxisSize.max,
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        FFButtonWidget(
+                          onPressed: () async {
+                            Navigator.pop(context);
+                          },
+                          text: 'Cancelar',
+                          options: FFButtonOptions(
+                            width: 120.0,
+                            height: 40.0,
+                            padding: EdgeInsets.all(8.0),
+                            iconPadding: EdgeInsetsDirectional.fromSTEB(
+                                0.0, 0.0, 0.0, 0.0),
+                            color: FlutterFlowTheme.of(context)
+                                .secondaryBackground,
+                            textStyle: FlutterFlowTheme.of(context)
+                                .bodyMedium
+                                .override(
                                   fontFamily: 'Inter',
                                   letterSpacing: 0.0,
                                 ),
-                        elevation: 0.0,
-                        borderSide: BorderSide(
-                          color: FlutterFlowTheme.of(context).alternate,
-                          width: 1.0,
-                        ),
-                        borderRadius: BorderRadius.circular(8.0),
-                      ),
-                    ),
-                    FutureBuilder<List<GetLoanByIdRow>>(
-                      future: SQLiteManager.instance.getLoanById(
-                        bookId: widget.id,
-                      ),
-                      builder: (context, snapshot) {
-                        // Customize what your widget looks like when it's loading.
-                        if (!snapshot.hasData) {
-                          return Center(
-                            child: SizedBox(
-                              width: 50.0,
-                              height: 50.0,
-                              child: CircularProgressIndicator(
-                                valueColor: AlwaysStoppedAnimation<Color>(
-                                  FlutterFlowTheme.of(context).primary,
-                                ),
-                              ),
+                            elevation: 0.0,
+                            borderSide: BorderSide(
+                              color: FlutterFlowTheme.of(context).alternate,
+                              width: 1.0,
                             ),
-                          );
-                        }
-                        final buttonGetLoanByIdRowList = snapshot.data!;
-
-                        return FFButtonWidget(
+                            borderRadius: BorderRadius.circular(8.0),
+                          ),
+                        ),
+                        FFButtonWidget(
                           onPressed: () async {
-                            if (buttonGetLoanByIdRowList
+                            if (containerGetLoanByIdRowList
                                     .firstOrNull?.isActivated ==
                                 1) {
                               ScaffoldMessenger.of(context).showSnackBar(
@@ -193,14 +196,14 @@ class _DialogDeleteBookWidgetState extends State<DialogDeleteBookWidget> {
                             ),
                             borderRadius: BorderRadius.circular(8.0),
                           ),
-                        );
-                      },
+                        ),
+                      ].divide(SizedBox(width: 12.0)),
                     ),
-                  ].divide(SizedBox(width: 12.0)),
+                  ].divide(SizedBox(height: 16.0)),
                 ),
-              ].divide(SizedBox(height: 16.0)),
-            ),
-          ),
+              ),
+            );
+          },
         ),
       ),
     );
